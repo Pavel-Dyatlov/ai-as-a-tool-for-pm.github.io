8752dad6f47abb7d524a21e17609a48efd546f93
Pavel-Dyatlov <140374225+Pavel-Dyatlov@users.noreply.github.com>
2023-08-08 23:24:50 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu__list');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+});
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
@@ -7,4 +14,33 @@ const swiper = new Swiper(".swiper", {
     delay: 5500,
     disableOnInteraction: false,
   },
-});
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
